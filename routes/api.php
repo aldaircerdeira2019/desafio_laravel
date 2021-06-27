7605ff0eaa1d11bb12cfb7290f620a51de0753e6
aldair cerdeira <aldaircerdeira10@hotmail.com>
2021-06-27 14:30:38 -0400

    class controller auth

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\{ RegisterController, LoginController };
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->get('/athenticated', function (Request $request) {
+    return response()->json($request->user());
 });
+
+Route::post('register' , [RegisterController::class, 'register']);
+Route::post('login' , [LoginController::class, 'login']);
+Route::post('logout' , [LoginController::class, 'logout']);
