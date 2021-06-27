0f14b5402cbb7e12633b0726d9acb247611be8a3
aldair cerdeira <aldaircerdeira10@hotmail.com>
2021-06-27 14:43:55 -0400

    populando o banco

---
+++ database/factories/DicaFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Dica;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Modelo;
+use App\Models\User;
+
+class DicaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Dica::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $modelo_id = Modelo::pluck('id')->toArray();
+        $user_id = User::pluck('id')->toArray();
+
+        return [
+            'title'         => $this->faker->paragraph(1),
+            'descri'        => $this->faker->text(),
+            'modelo_id'     => $this->faker->randomElement($modelo_id), 
+            'user_id'       => $this->faker->randomElement($user_id), 
+        ];
+    }
+}
