8f6e126e05236078cd3a99af794eb1bb4a9e5f8c
aldair cerdeira <aldaircerdeira10@hotmail.com>
2021-06-27 14:50:55 -0400

    home retornando as dicas com consultas joins

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\{ RegisterController, LoginController };
+use App\Http\Controllers\{ HomeController };
 
 Route::middleware('auth:sanctum')->get('/athenticated', function (Request $request) {
     return response()->json($request->user());
@@ -11,3 +12,5 @@ Route::middleware('auth:sanctum')->get('/athenticated', function (Request $reque
 Route::post('register' , [RegisterController::class, 'register']);
 Route::post('login' , [LoginController::class, 'login']);
 Route::post('logout' , [LoginController::class, 'logout']);
+
+Route::get('home' , [HomeController::class, 'index']);
